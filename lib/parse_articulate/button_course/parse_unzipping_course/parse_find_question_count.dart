54d79ff67c6cca199ca2983564242ace0f884c20
Алексей Полесов <a.polesov@mail.ru>
2020-12-07 18:55:09 +0400

    change url parse analysis

--- lib/parse_articulate/button_course/parse_unzipping_course/parse_find_question_count.dart
+++ lib/parse_articulate/button_course/parse_unzipping_course/parse_find_question_count.dart
@@ -100,6 +100,11 @@ class ParseFindQuestionCount {
       }
 
       loadNextQuiz();
+    } else {
+      statusLoad = StatusParseUnzippingStatus.finishParseQuiz;
+      onFinishedParse(_quizUrls);
+      _controller.stopLoading();
+      _controller.clearCache();
     }
   }
 
@@ -109,12 +114,6 @@ class ParseFindQuestionCount {
 
     var searchStartButton = documentHtml.getElementsByClassName(_classButton);
     if (searchStartButton.isNotEmpty) {
-    /*  statusLoad = StatusParseUnzippingStatus.clickStartQuiz;
-      String clickButton = """
-      document.getElementsByClassName("quiz-header__start-quiz brand--color").click = function(){ console.log("click button");};
-      document.getElementsByClassName("quiz-header__start-quiz brand--color").click();""";
-      _controller.evaluateJavascript(source: clickButton);
-*/
       var searchQuestionCounter =
           documentHtml.getElementsByClassName(_classCounterLabel);
       if (searchQuestionCounter.isNotEmpty) {
